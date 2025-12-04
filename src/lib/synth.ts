dea6a7fd41e9fcbb6d19f2e2396ee8bce3185453
Prasanta Karmakar <prasanta.ani@gmail.com>
2025-12-04 10:42:47 +0000

    Grand piano tone is not sounds like a real grand piano

--- src/lib/synth.ts
+++ src/lib/synth.ts
@@ -18,13 +18,15 @@ const instruments = {
 const instrumentConfigs = {
     grandPiano: {
         oscillator: {
-            type: "sine"
+            type: "fatsawtooth",
+            count: 3,
+            spread: 30
         },
         envelope: {
-            attack: 0.005,
-            decay: 0.1,
-            sustain: 0.3,
-            release: 1
+            attack: 0.01,
+            decay: 0.2,
+            sustain: 0.5,
+            release: 1.5,
         }
     }
 }
