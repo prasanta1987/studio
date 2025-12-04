5ad139d39bfc7eb96de207548d0e48dcb321c94b
Prasanta Karmakar <prasanta.ani@gmail.com>
2025-12-04 09:59:10 +0000

    Try fixing this error: `Console SecurityError: Failed to execute 'reques

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta httpEquiv="Permissions-Policy" content="midi=*" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
